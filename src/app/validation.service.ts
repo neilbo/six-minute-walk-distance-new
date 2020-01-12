2c955761907d27b8b4713015a0aeb271ee37f716
Neil <neilbo21@gmail.com>
2020-01-12 15:51:33 +1000

    Tidy up validation service

--- src/app/validation.service.ts
+++ src/app/validation.service.ts
@@ -1,8 +1,8 @@
-import * as _ from 'lodash';
+import * as _ from "lodash";
 import { AbstractControl } from "@angular/forms";
 
 export enum ValidationErrors {
-  REQUIRED = "required",
+  REQUIRED = "required"
   // INVALID_EMAIL_ADDRESS = "invalidEmailAddress",
   // INVALID_PASSWORD = "invalidPassword",
   // INVALID_AUS_PHONE = "invalidAusPhone",
@@ -24,41 +24,17 @@ export type ValidationResult = { [I in ValidationErrors]?: any } | null;
 export type Validator = (control: AbstractControl) => ValidationResult;
 
 export class ValidationService {
-  // static getValidatorErrorMessage(validatorName: string, validatorValue?: any, validatorInstance?: any) {
-  //   const config = {
-  //     'required': `This is required`,
-  //     'required_age': `Please enter an age`,
-  //     'required_height_cm': `Please enter a height in cm`,
-  //     'required_weight_lbs': `Please enter a weight in lbs`,
-  //     'required_weight_kg': `Please enter a weight in kg`,
-  //     'required_height_feet': `Please enter a height in feet`,
-  //     'required_height_inch': `Please enter a height in inches`,
-  //     'invalidCreditCard': 'Please enter valid credit card number',
-  //     'invalidEmailAddress': 'Please enter a valid email address',
-  //     'invalidPassword': 'Please enter a valid password. Password must be at least 6 characters long, and contain a number.',
-  //     'invalid_age': 'Please enter a valid age',
-  //     'invalidInches': 'Please enter a valid height in inches',
-  //     'minlength': `Too short. Please enter at lease ${validatorValue.requiredLength} characters`
-  //   };
-
-  //   return config[validatorName];
-  // }
-
+  
   static getValidatorErrorMessage(
     validatorName: string,
     fieldName: string,
-    validatorValue?: any,
+    validatorValue?: any
   ) {
     const config: ValidationMessages = {
-      required: `${fieldName} is required`,
+      required: `${fieldName} is required`
       // invalidEmailAddress: `${fieldName} is not formatted correctly`,
       // invalidPassword:
       //   "Password must be at least 8 characters long, and contain a number", // TODO: Probably won't use password field
-      // invalidAusPhone: `${fieldName} Must be 10 digits starting with '04'`,
-      // invalidDate: "Must be a valid date", // Should probably never happen in normal circumstances
-      // dateNotInPast: `${fieldName} must be a date in the past`,
-      // dateNotInFuture: `${fieldName} must be a date in the future`,
-      // medicineDailyOccurrence: `${fieldName} must be a number between 1 and 12, or 24`,
       // wholeNumber: `${fieldName} must be a whole number (no decimals)`,
       // notANumber: `${fieldName} must be number`,
       // invalidSystolic: `${fieldName} must be between ${SYSTOLIC_LOWER_LIMIT} and ${SYSTOLIC_UPPER_LIMIT}`,
@@ -75,27 +51,35 @@ export class ValidationService {
     const val = control.value;
 
     if (!_.isNumber(val) && val !== true && _.isEmpty(val)) {
-      return { required: true };
+      // return { required: true };
+      return { [ValidationErrors.REQUIRED]: true };
     }
     return null;
   }
 
-
   static creditCardValidator(control) {
     // Visa, MasterCard, American Express, Diners Club, Discover, JCB
-    if (control.value.match(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/)) {
+    if (
+      control.value.match(
+        /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
+      )
+    ) {
       return null;
     } else {
-      return { 'invalidCreditCard': true };
+      return { invalidCreditCard: true };
     }
   }
 
   static emailValidator(control) {
     // RFC 2822 compliant regex
-    if (control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+    if (
+      control.value.match(
+        /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+      )
+    ) {
       return null;
     } else {
-      return { 'invalidEmailAddress': true };
+      return { invalidEmailAddress: true };
     }
   }
 
@@ -105,7 +89,7 @@ export class ValidationService {
     if (control.value.match(/^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,100}$/)) {
       return null;
     } else {
-      return { 'invalidPassword': true };
+      return { invalidPassword: true };
     }
   }
 
@@ -116,65 +100,10 @@ export class ValidationService {
     const notANumber = isNaN(control.value);
 
     if (notWholeNumber || tooOld || tooYoung || notANumber) {
-      return { 'invalid_age': true };
+      return { invalid_age: true };
     } else {
       return null;
     }
-  }
-
-  static ageRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-
-    if (isEmpty) {
-      return { 'required_age': true };
-    }
-    return null;
-  }
-
-  static cmRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_height_cm': true };
-    }
-    return null;
-  }
-
-  static feetValidate(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_height_feet': true };
-    }
-    return null;
-  }
-  static kgRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_weight_kg': true };
-    }
-    return null;
-  }
-  static lbsRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_weight_lbs': true };
-    }
-  }
-
-  static genderRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_gender': true };
-    }
-    return null
-  }
-
-  static inchesRequired(control) {
-    const isEmpty = _.isEmpty(control.value);
-    if (isEmpty) {
-      return { 'required_height_inch': true };
-    }
-    return null;
-
   }
 
   static inchesValidate(control) {
@@ -182,10 +111,9 @@ export class ValidationService {
     const notANumber = isNaN(control.value);
     const notWholeNumber = control.value % 1 !== 0;
     if (notWholeNumber || !betweenZeroAndEleven || notANumber) {
-      return { 'invalidInches': true };
+      return { invalidInches: true };
     } else {
       return null;
     }
-
   }
 }
