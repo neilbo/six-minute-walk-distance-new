66f7c42c75366af4a08ddfe8fdf2b20e977658d7
Neil <neilbo21@gmail.com>
2020-01-13 03:34:27 +1000

    #5 Style with Autofocus.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,24 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder } from "@angular/forms";
 import { debounceTime } from "rxjs/operators";
 import { ValidationService } from "../validation.service";
 import isEmpty from "lodash/isEmpty";
 import { CalculateDistanceService } from "../calculate-distance.service";
+import { IonInput, ToastController } from "@ionic/angular";
+import copyToClipboard from "../utils/copy-to-clipboard";
 
 @Component({
   selector: "app-home",
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild("height", { static: false }) height: IonInput;
   metricForm: FormGroup;
   constructor(
     public formBuilder: FormBuilder,
-    public calculateDistance: CalculateDistanceService
+    public calculateDistance: CalculateDistanceService,
+    public toastCtrl: ToastController
   ) {
     this.metricForm = this.formBuilder.group({
       cm: [
@@ -31,7 +35,10 @@ export class HomePage {
       ],
       gender: ["", [ValidationService.isRequired]]
     });
-    // this.onFormChange();
+  }
+
+  ngOnInit() {
+    this.focusOnInput();
   }
 
   showResults() {
@@ -46,8 +53,11 @@ export class HomePage {
       this.metricForm.controls.gender.value
     );
   }
+
   resetForm() {
     this.metricForm.reset();
+    this.copyToClipboard(this.getDistance());
+    this.focusOnInput();
   }
 
   onFormChange() {
@@ -65,5 +75,28 @@ export class HomePage {
       isEmpty(this.metricForm.controls.kg.value) ||
       isEmpty(this.metricForm.controls.gender.value)
     );
+  }
+
+  focusOnInput() {
+    setTimeout(() => {
+      if (this.height) {
+        this.height.setFocus();
+      }
+    }, 2000);
+  }
+
+  copyToClipboard(val: any) {
+    copyToClipboard(val);
+    this.presentCopiedToast(val);
+  }
+
+  async presentCopiedToast(val?: any): Promise<void> {
+    const toast = await this.toastCtrl.create({
+      message: `  <ion-icon name="copy"></ion-icon> Copied ${val} to Clipboard!`,
+      duration: 2500,
+      cssClass: "toast-default",
+      position: `middle`
+    });
+    toast.present();
   }
 }
