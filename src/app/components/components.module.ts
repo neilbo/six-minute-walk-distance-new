285f335fa7fd5f084b5267e7d6c113a029a835f1
Neil <neilbo21@gmail.com>
2020-01-16 04:55:03 +1000

    Create Imperial Form component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from "@angular/core";
-import { ValidationErrorComponent } from './validation-error/validation-error.component';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { MetricFormComponent } from './metric-form/metric-form.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ValidationErrorComponent } from './validation-error/validation-error.component';
+import { ImperialFormComponent } from '../imperial-form/imperial-form.component';
+import { MetricFormComponent } from './metric-form/metric-form.component';
 
 @NgModule({
     imports: [IonicModule, CommonModule, ReactiveFormsModule, FormsModule],
-    declarations: [ValidationErrorComponent, MetricFormComponent, ],
-    exports: [ValidationErrorComponent, MetricFormComponent],
+    declarations: [ValidationErrorComponent, MetricFormComponent, ImperialFormComponent ],
+    exports: [ValidationErrorComponent, MetricFormComponent, ImperialFormComponent ],
 })
-
-
 
 export class ComponentsModule{}
---
+++ src/app/imperial-form/imperial-form.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'imperial-form',
+  templateUrl: './imperial-form.component.html',
+  styleUrls: ['./imperial-form.component.scss'],
+})
+export class ImperialFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
