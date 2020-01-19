ccfef466ba1408f585e865b5cd976dab1f6964bd
Neil <neilbo21@gmail.com>
2020-01-20 00:05:55 +1000

    Add scrollToTop  controls

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { IonContent } from "@ionic/angular";
+import toggleScrollButton from "../utils/toggle-scroll-button";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.page.html',
-  styleUrls: ['./about.page.scss'],
+  selector: "app-about",
+  templateUrl: "./about.page.html",
+  styleUrls: ["./about.page.scss"]
 })
 export class AboutPage implements OnInit {
-  title: string = 'About';
-  constructor() { }
+  title: string = "About";
+  isScrollEnabled: boolean = false;
+  @ViewChild(IonContent, { static: true }) content: IonContent;
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  onScroll(event: CustomEvent) {
+    this.isScrollEnabled = toggleScrollButton(event);
   }
 
+  scrollToTop() {
+    this.content.scrollToTop(300);
+  }
 }
